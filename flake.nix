2dc4c3aa8d51dbe401f745c60efedbb96a4b363b
Jacob Birkett <jacob_birkett@fourier.earth>
2024-12-16 22:17:07 -0800

    nix: flake: init -t nix-darwin

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Configurations for machines at Fourier";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nix-darwin }:
+    let
+      fourier-default-configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [ pkgs.vim ];
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in {
+      # Build this generic configuration using:
+      # $ darwin-rebuild build --flake .#fourier-default
+      darwinConfigurations."fourier-default" = nix-darwin.lib.darwinSystem {
+        modules = [ fourier-default-configuration ];
+      };
+    };
+}
