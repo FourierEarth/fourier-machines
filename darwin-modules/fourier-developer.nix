d199d130d7e46fb7de009df7557b73109042858f
Jacob Birkett <jacob_birkett@fourier.earth>
2025-01-07 16:40:12 -0600

    darwin-mods: default-darwin: enable touchid auth for sudo

--- darwin-modules/fourier-developer.nix
+++ darwin-modules/fourier-developer.nix
@@ -8,4 +8,8 @@
   environment.systemPackages = with pkgs; [ vim ];
 
   nix.linux-builder.enable = lib.mkDefault true;
+
+  # Only enabled for developers because normal users may not pay attention
+  # to what is asking for authentication.
+  security.pam.enableSudoTouchIdAuth = lib.mkDefault true;
 }
